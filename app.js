2648857cdc537a0432780a90b29e52bf1e7fee18
TouseefAbid <touseefabid737@gmail.com>
2024-07-17 15:39:34 +0500

    product image with change

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ async function getAllProducts(){
       createdBy} = product
       const productCards = `
       <div id="${data.id}" onclick="productDetails(this)" class="max-w-sm bg-white rounded-lg shadow-md overflow-hidden" data-id="${data.id}">
-          <img class="h-48 object-cover" style="width:300px" src="${productImage1}" alt="Product Image">
+          <img class="h-48 object-cover" style="width:100%;" src="${productImage1}" alt="Product Image">
           <div class="p-4">
               <h2 class="text-2xl font-semibold mb-2 text-capitalize">${productTitle}</h2>
               <p class="text-gray-700 mb-4">${productDesc}</p>
